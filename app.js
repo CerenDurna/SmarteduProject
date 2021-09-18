81d9154ddb023b2aa479b6aad8d44deb9b3b2b9f
unknown <ceren.durna@stu.khas.edu.tr>
2021-09-18 15:23:35 +0300

    lesson 2

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+const { render } = require('ejs');
 const express = require('express')
 
 const app = express();
 
+//template engine
+app.set("view engine","ejs")
+
+//midleware
+app.use(express.static("public"))
+
+//routes
 app.get('/', (req, res) => {
-    res.send('INDEX SAYFASI')
-}
-)
+    res.status(200).render('index',{
+        page_name:"index"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{
+        page_name:"about"
+    })
+})
 
 const port = 3000;
 app.listen(port, () => {
